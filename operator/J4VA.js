d107dc47de7cef464d9a936f8dfd1eaff155b235
ratatyi1a <rat82141@gmail.com>
2026-08-21 17:22:31 +0500

    operators

---
+++ operator/J4VA.js
@@ -0,0 +1,39 @@
+let a = 10;
+let b = 5;
+let c = 2;
+let sum= a + b + c;
+console.log(sum);
+let sum2= a + c - b;
+console.log(sum2)
+let sum3= a * b / c;
+console.log(sum3)
+let sum4= a % b;
+console.log(sum4)
+let x= 7;
+++x;
+console.log(x);
+let y= 7;
+--y;
+console.log(y);
+let z= 4;
+let w= 5;
+let sum5= z + w;
+let q="The result is: " + sum5;
+console.log(q);
+let e= 10;
+let r= 5;
+console.log(r < e > 0 < r);
+let t= 9;
+console.log(2==t<10);
+let sum6= e/r;
+console.log(sum6==5);
+let s= "10";
+console.log(s==a); // == сравнивает только значение
+console.log(s===a); // === сравнивает значение и тип данных
+let f=4;
+let h=7;
+console.log(f%2===0||f%3===0&&x%6!==0);
+console.log(h>10||y<5);
+
+
+
